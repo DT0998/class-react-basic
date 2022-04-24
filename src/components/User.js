a6476832639ec7e4e9f93b8f251fd80ffe0282a0
DT0998 <duongkytrieuvn@gmail.com>
2022-04-24 21:51:38 +0700

    convert userfinder class

--- src/components/User.js
+++ src/components/User.js
@@ -2,6 +2,9 @@ import classes from './User.module.css';
 import {Component} from 'react'
 
 class User extends Component {
+  componentDidMount(){
+    console.log("did mount");
+  }
   render(){
     return <li className={classes.user}>{this.props.name}</li>;
   }
